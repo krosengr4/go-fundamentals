43cc4545f7864bb6900cd2b00f5d8b61d494b304
krosengr4 <rosenkev4@gmail.com>
2025-08-19 13:33:14 -0500

    Created a basic web service using Go

--- web-service/main.go
+++ web-service/main.go
@@ -1,9 +1,23 @@
 package main
 
+import(
+	"os"
+	"net/http"
+	"io"
+)
+
 func main() {
+	// Handle the requests to the "/" path
+	http.HandleFunc("/", Handler)
+	// Using nil for the handler will use the DefaultServeMux provided by Go
+	http.ListenAndServe(":3000", nil)
 
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	
+	// Using os.Open we can get the the menu.txt file
+	f, _ := os.Open("./menu.txt")
+
+	// io.Copy allows us to copy from a read source to a write source
+	io.Copy(w, f)
 }
