1e2bb4b657c7a4300fed48300c1c0119cc5bf376
Kevin Rosengren <rosenkev4@gmail.com>
2025-08-22 13:54:44 -0500

    Added notes on using structs in go

--- aggregate-data-types/structs/structs.go
+++ aggregate-data-types/structs/structs.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	basicStruct()
+	customType()
 }
 
 func basicStruct() {
@@ -12,9 +13,33 @@ func basicStruct() {
 		age  int
 	}
 
-	fmt.Println(a) //{ 0}
+	fmt.Println("Empty struct:\n", a) //{ 0}
 
 	a.name = "Mike"
 	a.age = 102
-	fmt.Println(a) //{Mike 102}
+	fmt.Println("Struct after giving values:\n", a) //{Mike 102}
+}
+
+func customType() {
+	// Creating a custom type based on struct. keyword = type
+	type myStruct struct {
+		name string
+		age  int
+	}
+
+	// Assign value with a struct literal
+	var a = myStruct{
+		name: "Jasper",
+		age:  508}
+
+	fmt.Println("Original struct:\n", a)
+
+	// Copy the value of a into a2 (structs = value type)
+	a2 := a
+	// Make a change in new struct (a2)
+	a2.name = "Jaff" //<--- The value of a will not change
+
+	fmt.Println("Original struct(a) after change to a2:\n", a)
+	fmt.Println("a2 after change:\n", a2)
+
 }
